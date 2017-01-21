f7f04d4baba5b44a6a91d7a9d850bdd6ec84d230
vishrutkohli <kohlivishrut@gmail.com>
2017-01-21 13:35:41 +0530

    fuck appathon

--- isgw/Smartelec/models.py
+++ isgw/Smartelec/models.py
@@ -26,7 +26,5 @@ class Dailylog(models.Model):
 	day=models.IntegerField(default=0,unique=True)
 	month=models.IntegerField(default=0)
 	year=models.IntegerField(default=0)
-	predicted_low=models.IntegerField(default=0)
-	current_rate=models.IntegerField(default=0)
 	
 
--- isgw/isgw/urls.py
+++ isgw/isgw/urls.py
@@ -7,4 +7,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^weather$', 'Smartelec.views.datagerator', name='datagerator') , 
+
+    url(r'^rate$', 'Smartelec.views.realgenerator', name='realgenerator') , 
+    url(r'^ideal$', 'Smartelec.views.ideal_rate', name='realgenerator') , 
 )
